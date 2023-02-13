a5db229fc009fa877b0fa12f8d3fddc14b780b55
Akhil Kumar M <121224853+Akhi-26@users.noreply.github.com>
2023-02-13 14:07:21 +0530

    Add files via upload

--- Cities UI.dart
+++ Cities UI.dart
@@ -1,5 +1,19 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: cities(),
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+      )));
+}
 
 class cities extends StatelessWidget {
   var place = <String>[
